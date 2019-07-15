96cb52ab82ff6310ea0f5b351ad202ec21ad00ab
sonjy <thswldus1994@naver.com>
2019-07-15 18:00:06 +0900

    add todo menu to navbar

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -15,12 +15,13 @@
         />
 
         <sidebar-item :link="{name: 'Icons', icon: 'ni ni-planet text-blue', path: '/icons'}"/>
+        <sidebar-item :link="{name: 'Todo', icon: 'ni ni-check-bold text-green', path: '/todo'}"/>
+        <sidebar-item :link="{name: 'Malwares', icon: 'ni ni-bullet-list-67 text-red', path: '/malwares'}"/>
         <sidebar-item :link="{name: 'Maps', icon: 'ni ni-pin-3 text-orange', path: '/maps'}"/>
         <sidebar-item :link="{name: 'User Profile', icon: 'ni ni-single-02 text-yellow', path: '/profile'}"/>
         <sidebar-item :link="{name: 'Tables', icon: 'ni ni-bullet-list-67 text-red', path: '/tables'}"/>
         <sidebar-item :link="{name: 'Login', icon: 'ni ni-key-25 text-info', path: '/login'}"/>
         <sidebar-item :link="{name: 'Register', icon: 'ni ni-circle-08 text-pink', path: '/register'}"/>
-
       </template>
     </side-bar>
     <div class="main-content" :data="sidebarBackground">
